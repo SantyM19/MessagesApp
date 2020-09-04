a96edfccc983915b00d7f1bc9cd9d47fa0b79c49
Santiago <santiagomejiad19@hotmail.com>
2020-09-03 21:23:01 -0500

    Read DB
    We read the DB inf

--- src/main/java/mensajesService.java
+++ src/main/java/mensajesService.java
@@ -25,7 +25,7 @@ public class mensajesService {
     }
     
     public static void listarMensajes(){
-        
+        MensajesDAO.leerMensajesDB();
     }
     public static void borrarMensaje(){
         
